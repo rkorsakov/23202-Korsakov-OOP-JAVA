76ce9f48b3f503c4844fe9359ae8b3f0e489907d
Roman <roma3io@mail.ru>
2025-03-31 17:37:00 +0700

    Storage implementation

--- task-4/src/main/java/factory/storage/Storage.java
+++ task-4/src/main/java/factory/storage/Storage.java
@@ -1,4 +1,48 @@
 package factory.storage;
 
-public class Storage {
+import factory.product.Product;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Storage<T extends Product> {
+    private final Queue<T> products;
+    private final int capacity;
+    private final Object lock = new Object();
+
+    public Storage(int capacity) {
+        this.capacity = capacity;
+        this.products = new LinkedList<>();
+    }
+
+    public void put(T product) throws InterruptedException {
+        synchronized (lock) {
+            while (products.size() >= capacity) {
+                lock.wait();
+            }
+            products.add(product);
+            lock.notifyAll();
+        }
+    }
+
+    public T take() throws InterruptedException {
+        synchronized (lock) {
+            while (products.isEmpty()) {
+                lock.wait();
+            }
+            T product = products.poll();
+            lock.notifyAll();
+            return product;
+        }
+    }
+
+    public int getSize() {
+        synchronized (lock) {
+            return products.size();
+        }
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
