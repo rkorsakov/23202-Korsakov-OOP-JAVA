900298fc4063f4ceb3b20540129a706a4395b6cd
Roman <roma3io@mail.ru>
2025-03-31 17:15:14 +0700

    better logic for config file

--- task-4/src/main/java/factory/config/FactoryConfig.java
+++ task-4/src/main/java/factory/config/FactoryConfig.java
@@ -1,60 +1,48 @@
 package factory.config;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class FactoryConfig {
-    private final int bodyStorageSize;
-    private final int engineStorageSize;
-    private final int detailStorageSize;
-    private final int carStorageSize;
-    private final int numAccessorySuppliers;
-    private final int numWorkers;
-    private final int numDealers;
-    private final boolean logSale;
+    private final Properties properties;
 
-    public FactoryConfig(String configFile) throws Exception {
-        Properties props = new Properties();
-        props.load(new FileInputStream(configFile));
-        this.bodyStorageSize = Integer.parseInt(props.getProperty("StorageBodySize", "100"));
-        this.engineStorageSize = Integer.parseInt(props.getProperty("StorageMotorSize", "100"));
-        this.detailStorageSize = Integer.parseInt(props.getProperty("StorageAccessorySize", "100"));
-        this.carStorageSize = Integer.parseInt(props.getProperty("StorageAutoSize", "100"));
-        this.numAccessorySuppliers = Integer.parseInt(props.getProperty("AccessorySuppliers", "5"));
-        this.numWorkers = Integer.parseInt(props.getProperty("Workers", "10"));
-        this.numDealers = Integer.parseInt(props.getProperty("Dealers", "20"));
-        this.logSale = Boolean.parseBoolean(props.getProperty("LogSale", "true"));
+    public FactoryConfig(String configPath) throws IOException {
+        properties = new Properties();
+        try (FileInputStream fis = new FileInputStream(configPath)) {
+            properties.load(fis);
+        }
     }
 
-    public int getBodyStorageSize() {
-        return bodyStorageSize;
+    public int getStorageBodySize() {
+        return Integer.parseInt(properties.getProperty("StorageBodySize"));
     }
 
-    public int getEngineStorageSize() {
-        return engineStorageSize;
+    public int getStorageEngineSize() {
+        return Integer.parseInt(properties.getProperty("StorageEngineSize"));
     }
 
-    public int getDetailStorageSize() {
-        return detailStorageSize;
+    public int getStorageAccessorySize() {
+        return Integer.parseInt(properties.getProperty("StorageAccessorySize"));
     }
 
-    public int getCarStorageSize() {
-        return carStorageSize;
+    public int getStorageAutoSize() {
+        return Integer.parseInt(properties.getProperty("StorageAutoSize"));
     }
 
-    public int getNumAccessorySuppliers() {
-        return numAccessorySuppliers;
+    public int getAccessorySuppliersCount() {
+        return Integer.parseInt(properties.getProperty("AccessorySuppliers"));
     }
 
-    public int getNumWorkers() {
-        return numWorkers;
+    public int getWorkersCount() {
+        return Integer.parseInt(properties.getProperty("Workers"));
     }
 
-    public int getNumDealers() {
-        return numDealers;
+    public int getDealersCount() {
+        return Integer.parseInt(properties.getProperty("Dealers"));
     }
 
     public boolean isLogSale() {
-        return logSale;
+        return Boolean.parseBoolean(properties.getProperty("LogSale"));
     }
 }
